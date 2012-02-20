37d8484246ae12e4495aa0cbe55ef657d54f8bef
Marcos Tapajos <tapajos@gmail.com>
2012-02-20 19:09:42 -0200

    Calculate average

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,8 @@
 class City < ActiveRecord::Base
   validates_presence_of :name, :state
   belongs_to :state
+  has_many :salaries
+  def average
+    self.salaries.average(:amount) || 0.0
+  end
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe City do
-  fixtures :states
+  fixtures :states, :cities
   it { should have_valid(:name).when('Rio de Janeiro') }
   it { should_not have_valid(:name).when('') }
   it { should_not have_valid(:name).when(nil) }
 
   it { should have_valid(:state).when(states(:rio_de_janeiro)) }
   it { should_not have_valid(:state).when(nil) }
+  
+  describe "average" do
+    let(:rio_de_janeiro) { cities(:rio_de_janeiro) }
+    it "should return the average of salaries" do
+      Salary.create!(:amount => 5000.00, :city => rio_de_janeiro)
+      Salary.create!(:amount => 1000.00, :city => rio_de_janeiro)
+      rio_de_janeiro.average.should == 3000.00
+    end
+    it "should return the zero when there are no salaries" do
+      rio_de_janeiro.average.should == 0.0
+    end
+  end
 end
