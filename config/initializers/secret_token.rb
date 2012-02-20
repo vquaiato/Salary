133ddaf2cb0bfaba94601b6afb70ebe92312b7e1
Marcos Tapajos <tapajos@gmail.com>
2012-02-20 18:22:34 -0200

    Create an empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salarios::Application.config.secret_token = '5dfdb2a4937d7627b5ee9bbac8d307e18ee4bdab6e747a17a67a9cb3e5230b9854f27f3c6b76dd5685cbb7c91bbf5e49bfc80dcc6c67fca7b92b1282841653f9'
