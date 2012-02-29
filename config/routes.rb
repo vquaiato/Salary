166cd073d5812ead49bd2e4febd83aa20a033b9d
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2012-02-29 13:12:48 -0300

    coming soon

--- config/routes.rb
+++ config/routes.rb
@@ -49,10 +49,10 @@ Salarios::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
-  resources :salaries do
-  end
-  get "/cities/in/:state" => "state#cities", as: :get_cities
-  post "/salary/create" => "salaries#create", as: :create_salary
+  # resources :salaries do
+  # end
+  # get "/cities/in/:state" => "state#cities", as: :get_cities
+  # post "/salary/create" => "salaries#create", as: :create_salary
 
   # See how all your routes lay out with "rake routes"
 
