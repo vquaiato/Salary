f4142e93fbeaf376e7c943c722af84918ac842c0
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2012-03-01 07:45:58 -0300

    removendo coming soon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 	def index
-		#@states = State.all
-		#@salario = Salary.new
-		#@salarios = Salary.grouped_by_cities
+		@states = State.all
+		@salario = Salary.new
+		@salarios = Salary.grouped_by_cities
 	end
 end
