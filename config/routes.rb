15b43322db37d8f7e03879804cc7cc47ee8f42f3
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2012-03-01 07:44:01 -0300

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Salarios::Application.routes.draw do
   end
   get "/cities/in/:state" => "state#cities", as: :get_cities
   post "/salary" => "salaries#create", as: :create_salary
->>>>>>> 63ef8e47055c0e14ba3d6cce88533cbc0d0cd0a3
 end
