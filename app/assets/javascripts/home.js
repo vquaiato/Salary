09067c67ad5683fb955799495b18d914e1355769
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2012-03-01 08:28:24 -0300

    máscara no input do modal

--- app/assets/javascripts/home.js
+++
@@ -1,5 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-$(function(){
-  $("#salary_amount").maskMoney({symbol:'R$ ', showSymbol:true, thousands:'.', decimal:',', symbolStay: true, allowZero:false, allowNegative:false})
-});
--- app/assets/javascripts/simplemodal/simplemodal.js
+++ app/assets/javascripts/simplemodal/simplemodal.js
@@ -3,6 +3,8 @@
 
 $(function(){
 	$("#informar_salario").click(function(e){
+		$("#osx-modal-content #salary_amount").maskMoney({symbol:'R$ ', showSymbol:true, thousands:'.', decimal:',', symbolStay: true, allowZero:false, allowNegative:false})
+		
 		OSX.init();
 		e.preventDefault();
 	});
