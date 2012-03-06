fa03b25659f2d6030550809215ad59c72b4a82b0
Rafael Felix <felix.rafael@gmail.com>
2012-03-05 22:06:23 -0300

    Ajustes no formulario de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Salarios::Application.routes.draw do
-  devise_for :admins
+  devise_scope :admin do
+    get '/admin', :to => "devise/sessions#new", :as => :admin_session
+    post '/admin', :to => "devise/sessions#create", :as => :admin_session
+    delete '/sign_out', :to => "devise/sessions#destroy", :as => :destroy_admin_session
+  end
 
   root :to => 'salaries#index'
   resources :salaries do
